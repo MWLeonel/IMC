8eaa641a1c7bcb8cf961d0155c0af6c246bebc99
MWLeonel <wmleonel@gmail.com>
2020-09-18 10:19:21 -0300

    Calcular IMC

---
+++ IMC/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura;
+            double peso;
+            double IMC;
+
+            Console.Write("Digite sua Altura(m): ");
+            altura = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite seu Peso(kg): ");
+            peso = double.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            IMC = peso / (altura * altura);
+            Console.WriteLine("Seu resultado de IMC é = {0} kg/m²", IMC);
+            Console.Write("\n");
+
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+        }
+    }
+}
